45d194b11078303942b1f0cc29c040fc826824c6
joao_a_fr <joaoadrielfr@gmail.com>
2026-05-25 16:56:01 -0300

    aprendendo js

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 60.1
+
+console.log(`A velocidade de seu carro é ${velocidade}Km/h`)
+if (velocidade > 60){
+    console.log("Vocé ultrapassou a velocidade permitida. MULTADO!") //condição simples
+}
+console.log('Dirija sempre com seu sinto de segurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'eua'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais.toUpperCase() == 'BRASIL' || pais == 'BRA'){
+    console.log('Brasileiro')
+} else{
+    console.log('Estrangeiro')
+}
